0325c252b3bae1cba795788c48fcaaeb3c4dc8f0
SandovalPablo <43177743+JosePablo24@users.noreply.github.com>
2019-03-05 17:14:28 -0600

    Update email.py

--- email.py
+++ email.py
@@ -4,6 +4,6 @@ client = sendgrid.SenGridClient("SG.idk2Az6DQymU3MtAfGUS5A.0R_VtN168B9bmWMmX2vKI
 message = sendrig.Mail()
 
 message.add_to("mmoreno@ids.upchiapas.edu.mx")
-message.set_from("173208@ids.upchiapas.edu.mx")
+message.set_from("joxesandoval.24@gmail.com")
 message.set_html("Prueba comd")
 client.send(message)
