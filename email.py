289efa5928f20c31bc2b39ebb257d49a1aeaad60
JosePablo24 <173198@ids.upchiapas.edu.mx>
2019-03-05 17:05:40 -0600

    ,

---
+++ email.py
@@ -0,0 +1,9 @@
+import sendgrid
+
+client = sendgrid.SenGridClient("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")
+message = sendrig.Mail()
+
+message.add_to("mmoreno@ids.upchiapas.edu.mx")
+message.set_from("173208@ids.upchiapas.edu.mx")
+message.set_html("Prueba comd")
+client.send(message)
